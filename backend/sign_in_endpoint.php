c3e95d41ae5d2e4aeab24cda6087ca7abd08a994
UltimateDev <iyideboluwatife8@gmail.com>
2025-01-18 03:40:13 +0100

    edit sign in

--- backend/sign_in_endpoint.php
+++ backend/sign_in_endpoint.php
@@ -1,3 +1,38 @@
 <?php
+     //Do not Touch, It works!
+    //Do not Touch, It works!
+    //Do not Touch, It works!
+    //Do not Touch, It works!
+    //Do not Touch, It works!
 
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); 
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json");
+
+    //database conn
+
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "luxedrive";
+
+    $conn = mysqli_connect($host, $user, $password, $dbname);
+
+   $user_email = filter_input(
+   INPUT_POST, 
+   'user_email', 
+   FILTER_SANITIZE_EMAIL
+);
+    $user_password = $_POST['user_password'];
+
+    $check = "SELECT * FROM users_data WHERE user_email = '$user_email'";
+    $result = mysqli_query($conn, $check);
+
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        $user_name = $row['user_name'];
+    }else{
+        echo"user not found";
+    }
 ?>
